9c998a7c682d815db1789196f5d39ed25efd324f
JordySamirC <jordy.cervera.dev@gmail.com>
2026-05-06 19:51:50 -0500

    first commit

---
+++ api-rest/hotel_cervera_api/src/main/java/com/hotel/cervera/hotel_cervera_api/HotelCerveraApiApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.cervera.hotel_cervera_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelCerveraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelCerveraApiApplication.class, args);
+	}
+
+}
---
+++ api-rest/hotel_cervera_api/src/test/java/com/hotel/cervera/hotel_cervera_api/HotelCerveraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel.cervera.hotel_cervera_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelCerveraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
