8e580c672859e2abfaca52e98c82d973bbd651c9
JordySamirC <jordy.cervera.dev@gmail.com>
2026-05-18 16:14:14 -0500

    refactor(bd): corregir triggers de auditoria y restaurar columnas de ingreso y correo
    - Eliminacion de triggers que usaban la columna 'updated_at'.
    - Adicion y poblacion de la columna 'correo_electronico' en la tabla 'usuarios'.
    - Adicion y poblacion de la columna 'fecha_ingreso' en la tabla 'estadias'.
    - Creacion de funcion de trigger y asignacion para la columna 'fecha_actualizacion'.
    - Remocion de codigo de migracion redundante en HotelCerveraApiApplication.

--- api-rest/hotel_cervera_api/src/main/java/com/hotel/cervera/hotel_cervera_api/HotelCerveraApiApplication.java
+++ api-rest/hotel_cervera_api/src/main/java/com/hotel/cervera/hotel_cervera_api/HotelCerveraApiApplication.java
@@ -2,8 +2,10 @@ package com.hotel.cervera.hotel_cervera_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class HotelCerveraApiApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +13,4 @@ public class HotelCerveraApiApplication {
 	}
 
 }
+
